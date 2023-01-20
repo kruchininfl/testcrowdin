f6b24aa4547440fc2522ad405d6707c1199add95
Artem Kruchinin <infamoustill@gmail.com>
2023-01-20 19:53:25 +0700

    i18n test 1

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App({onChangeLanguage}: { onChangeLanguage: (locale: "en" | "ru" | "sp"
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <Trans>Test english</Trans>
+                <Trans>Test 1</Trans>
             </header>
 
             <button onClick={() => onChangeLanguage("en")}>set en</button>
